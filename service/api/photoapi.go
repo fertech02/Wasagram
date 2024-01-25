b8403118f73fae2ac516ab10902231978af1dafc
Fernando Alonzi <alonzi.fernando209@gmail.com>
2024-01-25 10:48:01 +0100

    change photoapi.go

--- service/api/photoapi.go
+++ service/api/photoapi.go
@@ -7,10 +7,7 @@ import (
 // postPhoto handles the POST /photo API endpoint.
 func (rt *_router) postPhoto(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	
-}
 
-// getPhoto handles the GET /photo/:id API endpoint.
-func (rt *_router) getPhoto(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 }
 
 // deletePhoto handles the DELETE /photo/:id API endpoint.
