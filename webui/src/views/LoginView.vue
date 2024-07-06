75afb0fd146db1fda2bb6c7f3b4adcc65a8e2245
fertech02 <alonzifernando209@gmail.com>
2024-07-06 15:37:34 +0200

    adjusted login

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -1,3 +1,38 @@
+<script>
+export default {
+    data: function() {
+        return {
+            username: '',
+            errormsg: null,
+            Profile: {
+                username: '',
+                identifier: ''
+            }
+
+        }
+    },
+    methods: {
+        async doLogin() {
+            this.errormsg = null;
+            if (this.username == "") {
+                this.errormsg = "Please insert a username.";
+                return;
+            }
+            try {
+                let response = await this.$axios.post("/session", {username: this.username});
+                this.Profile = response.data;
+                localStorage.setItem("token", this.Profile.identifier);
+                localStorage.setItem("username", this.Profile.username);
+                this.$router.push({path: '/session'});
+            } catch (error) {
+                this.errormsg = error.response.data.message;
+            } 
+            this.loading = false;
+        },
+    }
+}
+</script>
+
 <template>
     <div
         class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -14,41 +49,6 @@
     <ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
 </template>
 
-<script>
-export default {
-    data: function() {
-        return {
-            username: '',
-            identifier: '',
-            loading: false,
-            errormsg: null,
-
-        }
-    },
-    methods: {
-        async doLogin() {
-            this.loading = true;
-            this.errormsg = null;
-
-            try {
-                let response = await this.$axios.post("/session", {username: this.username},{
-                    headers: {
-                        'Accept' : 'application/json',
-                        'Content-Type' : 'application/json'
-                    },
-                });
-                this.identifier = response.data.identifier;
-                localStorage.setItem("token", this.identifier);
-                localStorage.setItem("username", this.username);
-                this.$router.push({path: '/session'});
-            } catch (error) {
-                this.errormsg = error.response.data.message;
-            } 
-            this.loading = false;
-        },
-    }
-}
-</script>
 
 <style scoped>
     .input-group {
