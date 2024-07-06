ad42bcc97387b02ceef0890b65453a6e1ccb8286
fertech02 <alonzifernando209@gmail.com>
2024-07-06 12:57:55 +0200

    adjusted login

--- webui/src/views/LoginView.vue
+++ webui/src/views/LoginView.vue
@@ -1,11 +1,11 @@
 <template>
     <div
         class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Welcome to WASAPhoto</h1>
+        <h1 class="h2">Welcome to Wasaphoto</h1>
     </div>
     <div class="input-group mb-3">
         <input type="text" id="username" v-model="username" class="form-control"
-            placeholder="Insert a username to log in WASAPhoto." aria-label="Recipient's username"
+            placeholder="Insert a username to log in Wasaphoto." aria-label="Recipient's username"
             aria-describedby="basic-addon2">
         <div class="input-group-append">
             <button class="btn btn-success" type="button" @click="doLogin">Login</button>
@@ -51,31 +51,14 @@ export default {
 </script>
 
 <style scoped>
-.login-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-color: #f8f9fa;
-}
-
-.login-form {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    max-width: 400px;
-    margin: 0 auto;
-    padding: 20px;
-    background-color: #fff;
-    border-radius: 4px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-}
-
-.login-label {
-    padding: 3px;
-    display: block;
-    margin-bottom: 8px;
-}
+    .input-group {
+        width: 50%;
+        margin: auto;
+    }
+    .btn-success {
+        width: 100%;
+    }
+    .h2 {
+        margin: auto;
+    }
 </style>
