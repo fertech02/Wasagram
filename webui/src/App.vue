1391ea6387e56fff348e558e3e32b8db4beaa581
fertech02 <alonzifernando209@gmail.com>
2024-07-06 15:18:01 +0200

    new App.vue

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -1,86 +1,44 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
 
 <script>
-const token = sessionStorage.getItem('authToken');
-import { RouterLink, RouterView } from 'vue-router'
 export default {
 	data() {
 		return {
-			mypath: "/users/" + token,
-			streampath: "/users/" + token + "/stream/",
-		}
-	},
-	methods: {
-		logout() {
-			localStorage.clear();
-			sessionStorage.clear();
-			location.reload();
-		},
-	},
 
+		};
+	},
 }
+
 </script>
 
 <template>
-
-	<header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-		<a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#/">Example App</a>
-		<button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
-	</header>
-
-	<div class="container-fluid">
-		<div class="row">
-			<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
-				<div class="position-sticky pt-3 sidebar-sticky">
-					<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-						<span>General</span>
-					</h6>
-					<ul class="nav flex-column">
-						<li class="nav-item">
-							<RouterLink to="/" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#home"/></svg>
-								Home
-							</RouterLink>
-						</li>
-						<li class="nav-item">
-							<RouterLink to="/link1" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#layout"/></svg>
-								Menu item 1
-							</RouterLink>
-						</li>
-						<li class="nav-item">
-							<RouterLink to="/link2" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#key"/></svg>
-								Menu item 2
-							</RouterLink>
-						</li>
-					</ul>
-
-					<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
-						<span>Secondary menu</span>
-					</h6>
-					<ul class="nav flex-column">
-						<li class="nav-item">
-							<RouterLink :to="'/some/' + 'variable_here' + '/path'" class="nav-link">
-								<svg class="feather"><use href="/feather-sprite-v4.29.0.svg#file-text"/></svg>
-								Item 1
-							</RouterLink>
-						</li>
-					</ul>
+	<div>
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<div class="container-fluid">
+					<router-link to="/" class="navbar-brand">Wasaphoto</router-link>
+					<button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+						data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+						aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarNav">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<router-link to="/session" class="nav-link">Session</router-link>
+							</li>
+							<li class="nav-item">
+								<router-link to="/upload" class="nav-link">Upload</router-link>
+							</li>
+							<li class="nav-item">
+								<router-link to="/logout" class="nav-link">Logout</router-link>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</nav>
-
-			<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-				<RouterView />
-			</main>
-		</div>
+		</header>
 	</div>
 </template>
-
-<style>
-	.background {
-		background-color: #f8f9fa;
-		height: 100vh;
-	}
-</style>
