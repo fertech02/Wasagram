0608f98d76ce90679486ba88995e5740a5771a1e
fertech02 <alonzifernando209@gmail.com>
2024-07-07 15:27:48 +0200

    adjusted HomeView

--- webui/src/views/HomeView.vue
+++ webui/src/views/HomeView.vue
@@ -12,12 +12,13 @@ export default {
     data: function() {
         return {
             errorMsg: null,
-            successMsg: null,
+            successMsg: null,   
             Username: localStorage.getItem('username'),
             Uid: localStorage.getItem('token'),
             loading: false,
             image: null,
             images: null,
+            comment: '',
 
             // Comments
             Comments : {
@@ -168,7 +169,7 @@ export default {
             }
         },
 
-        async openComments() {
+        async openComments(Pid) {
             try {
                 let response = await this.$axios.get("/photos/" + Pid + "/comments", {
                     headers: {
@@ -232,6 +233,40 @@ export default {
             }
         },
 
+        searchUserUsername: "",
+        async SearchUser() {
+			if (this.searchUserUsername === this.username) {
+				this.errormsg = "You can't search yourself."
+			} else if (this.searchUserUsername === "") {
+				this.errormsg = "Emtpy username field."
+			} else {
+				try {
+					let response = await this.$axios.get("users/" + this.searchUserUsername + "/profile", {
+						headers: {
+							Authorization: "Bearer " + localStorage.getItem("token")
+						}
+					})
+					this.profile = response.data
+					this.$router.push({ path: '/users/' + this.searchUserUsername + '/view' })
+				} catch (e) {
+					if (e.response && e.response.status === 400) {
+						this.errormsg = "Form error, please check all fields and try again. If you think that this is an error, write an e-mail to us.";
+						this.detailedmsg = null;
+					} else if (e.response && e.response.status === 500) {
+						this.errormsg = "User does not exist on WASAPhoto.";
+						this.detailedmsg = e.toString();
+					} else {
+						this.errormsg = e.toString();
+						this.detailedmsg = null;
+					}
+				}
+			}
+		},
+
+        async ViewProfile() {
+            this.$router.push({path: '/users/' + this.Uid + '/profile'})
+        },
+
         async Logout() {
             localStorage.removeItem('username')
             localStorage.removeItem('Uid')
@@ -246,76 +281,95 @@ export default {
 </script>
 
 <template>
-    <div>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Home</h4>
-                        </div>
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="form-group">
-                                        <label for="image">Upload Photo</label>
-                                        <input type="file" ref="image" class="form-control" @change="uploadPhoto">
-                                    </div>
-                                    <button class="btn btn-primary" @click="submitPhoto">Submit</button>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="alert alert-danger" v-if="errorMsg" role="alert">
-                                        {{ errorMsg }}
-                                    </div>
-                                    <div class="alert alert-success" v-if="successMsg" role="alert">
-                                        {{ successMsg }}
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h4>Stream</h4>
-                                        </div>
-                                        <div class="card-body">
-                                            <div class="row">
-                                                <div class="col-md-12">
-                                                    <div class="alert alert-danger" v-if="errorMsg" role="alert">
-                                                        {{ errorMsg }}
-                                                    </div>
-                                                    <div class="alert alert-success" v-if="successMsg" role="alert">
-                                                        {{ successMsg }}
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="row">
-                                                <div class="col-md-12">
-                                                    <div class="card-columns">
-                                                        <div class="card" v-for="photo in Stream.photos" :key="photo.Pid">
-                                                            <img :src="photo.File" class="card-img-top" alt="...">
-                                                            <div class="card-body">
-                                                                <h5 class="card-title">Photo</h5>
-                                                                <p class="card-text">Date: {{ photo.Date }}</p>
-                                                                <button class="btn btn-primary" @click="openComments(photo.Pid)">Comments</button>
-                                                                <button class="btn btn-primary" @click="likePhoto(photo.Pid, Uid)">Like</button>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <success-msg ref="successMsg" :successMsg="successMsg"></success-msg>
-        <comment-modal ref="commentModal" :comments="Comments.comments" @comment="commentPhoto"></comment-modal>
-    </div>
+	<div>
+		<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+			<div class="position-sticky pt-3 sidebar-sticky">
+				<h6
+					class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+					<span>General</span>
+				</h6>
+				<ul class="nav flex-column">
+					<li class="nav-item">
+						<RouterLink to="/session" class="nav-link">
+							<svg class="feather">
+								<use href="/feather-sprite-v4.29.0.svg#home" />
+							</svg>
+							Home
+						</RouterLink>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<div
+			class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+			<h1 class="h2">Welcome back {{this.username }}</h1>
+			<div class="btn-toolbar mb-2 mb-md-0">
+				<div class="btn-group me-2">
+					<button class="btn btn-danger" type="button" @click="Logout">Logout</button>
+					<button class="btn btn-primary" type="button" @click="ViewProfile">Profile</button>
+					<input type="file" accept="image/*" class="btn btn-outline-primary" @change="uploadPhoto" ref="file">
+					<button class="btn btn-success" @click="submitPhoto">Upload</button>
+				</div>
+			</div>
+		</div>
+		<div class="input-group mb-3">
+
+			<input type="text" id="searchUserUsername" v-model="searchUserUsername" class="form-control"
+				placeholder="Search a user in WASAPhoto." aria-label="Recipient's username"
+				aria-describedby="basic-addon2">
+			<div class="input-group-append">
+				<button class="btn btn-primary" type="button" @click="SearchUser">Search</button>
+			</div>
+		</div>
+
+		<div
+			class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+		</div>
+
+		<ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
+		<SuccessMsg v-if="successmsg" :msg="successmsg"></SuccessMsg>
+
+		<LogModal id="logviewer" :log="Comments" :token="token"></LogModal>
+
+
+		<div class="row">
+			<div class="col-md-4" v-for="photo in Stream.photos" :key="photo.Pid">
+				<div class="card mb-4 shadow-sm">
+					<img class="card-img-top" :src=photo.File alt="Card image cap">
+					<div class="card-body">
+						<RouterLink :to="'/users/' + photo.Uid + '/view'" class="nav-link">
+							<button type="button" class="btn btn-outline-primary">{{photo.Uid}}</button>
+						</RouterLink>
+						<div
+							class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+						</div>
+						<p class="card-text">Uploaded on : {{photo.Date}}</p>
+
+						<div class="input-group mb-3">
+							<input type="text" id="comment" v-model="comment" class="form-control"
+								placeholder="Comment!" aria-label="Recipient's username"
+								aria-describedby="basic-addon2">
+							<div class="input-group-append">
+								<button class="btn btn-primary" type="button"
+									@click="commentPhoto(photo.Uid, photo.Pid, photo.Message)">Send</button>
+							</div>
+						</div>
+
+						<div class="d-flex justify-content-between align-items-center">
+							<div class="btn-group">
+								<button type="button" class="btn btn-dark"
+									@click="openComments(photo.Pid)">Comments</button>
+								<button type="button"  class="btn btn-primary"
+									@click="likePhoto(photo.Pid, photo.Uid)">Like</button>
+								<button type="button"  class="btn btn-danger"
+									@click="unlikePhoto(photo.Pid, photo.Uid)">Unlike</button>
+							</div>
+						</div>
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
