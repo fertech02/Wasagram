64bdf7ce522ecfe0b887f1cbed2ca74743942e31
fertech02 <alonzi.1992758@studenti.uniroma1.it>
2024-11-20 11:45:20 +0100

    adjusted photo delete

--- service/filesystem/remove-photo.go
+++ service/filesystem/remove-photo.go
@@ -3,11 +3,14 @@ package filesystem
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
-func RemovePhoto(pid string) error {
+const uploadDirectory = "/tmp/filesystem/"
 
-	filename := fmt.Sprintf("/tmp/filesystem/%s.jpg", pid)
+func RemovePhoto(photoID string) error {
+
+	filename := filepath.Join(uploadDirectory, fmt.Sprintf("%s%s", photoID, ".jpg"))
 	err := os.Remove(filename)
 	if err != nil {
 		return fmt.Errorf("removing file: %w", err)
